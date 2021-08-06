43bc83208640c43db39ed09c8fe07f6df0c0ca76
kalashshah <81062983+kalashshah@users.noreply.github.com>
2021-08-06 19:33:18 +0530

    Added Bootswatch Slate Theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Header />
+      <h1>Content</h1>
+      <Footer />
+    </>
   );
 }
 
