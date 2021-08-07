4afe48a65b4e14f0f66f534e0acdb90d4d4ace9c
Vijay-K-2003 <vijaykaranjkar2003@gmail.com>
2021-08-07 12:12:50 +0530

    resolved errors

---
+++ src/dishes.js
@@ -0,0 +1,16 @@
+const dishes = [
+  {
+    _id: "1",
+    name: "Margherita",
+    image: "/images/margherita.jpg",
+    description:
+      "A hugely popular margherita, with a deliciously tangy single cheese topping",
+    restraunt : "Domino's",
+    cuisine: "Italian",
+    price: 120,
+    rating: 4.0,
+    reviews : 121,
+  },
+];
+
+export default dishes;
